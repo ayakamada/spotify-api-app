171c5476fa8e71090aa0b6e240918aeb42140819
ayakamada <sdkamada@gmail.com>
2023-03-11 20:39:16 +0900

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -47,14 +47,18 @@ async function refreshAccessToken(token) {
   }
 }
 
+const scope =
+  "user-read-recently-played user-read-playback-state user-top-read user-modify-playback-state user-read-currently-playing user-follow-read playlist-read-private user-read-email user-read-private user-library-read playlist-read-collaborative";
+
 export default NextAuth({
   // 1つまたは複数の認証プロバイダを設定する
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      authorization:
-        "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-read-email,streaming,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state,user-read-recently-played,user-follow-read",
+      authorization: {
+        params: { scope },
+      },
     }),
     // ...他にも認証プロバイダがあればここから追加する。
   ],
