2dff31c2bafc6d9da981a998174edcee8e40b177
ayakamada <sdkamada@gmail.com>
2023-03-30 11:26:46 +0900

    Update spotify.js

--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -6,16 +6,32 @@ const TOP_TRACKS_ENDPOINT_MEDIUM = `https://api.spotify.com/v1/me/top/tracks?lim
 export const TOP_TRACKS_ENDPOINT_LONG = `https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=long_term`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
-/**
- * Get a User's Top Tracks
- * https://developer.spotify.com/documentation/web-api/reference/personalization/get-users-top-artists-and-tracks/
- */
+// /**
+//  * Get a User's Top Tracks
+//  * https://developer.spotify.com/documentation/web-api/reference/personalization/get-users-top-artists-and-tracks/
+//  */
+// export const getTopTracksShort = async (session) => {
+//   const res = await fetch(TOP_TRACKS_ENDPOINT_SHORT, {
+//     headers: {
+//       Authorization: `Bearer ${session.accessToken}`,
+//     },
+//   }).then((res) => res.json());
+
+//   return res;
+// };
+
+// エンドポイント"TOP_TRACKS_ENDPOINT_SHORT"からトラック情報を取得する
+// トラック情報は"res"に格納される
+// トラック情報を返す
+// axiosを使用する場合
 export const getTopTracksShort = async (session) => {
-  const res = await fetch(TOP_TRACKS_ENDPOINT_SHORT, {
-    headers: {
-      Authorization: `Bearer ${session.accessToken}`,
-    },
-  }).then((res) => res.json());
+  const res = await axios
+    .get(TOP_TRACKS_ENDPOINT_SHORT, {
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+    })
+    .then((res) => res.data);
 
   return res;
 };
