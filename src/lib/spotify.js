b8414517197505145b1ffba869ed321216740fca
ayakamada <sdkamada@gmail.com>
2023-04-07 11:42:47 +0900

    header記述が増えるのでfetch用関数を共通で使用する

--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -1,37 +1,51 @@
 import axios from "axios";
 
+//https://spotify-profile.herokuapp.com/
+
 // ENDPOINTS ------------------------------------------------------------------------------
-const TOP_TRACKS_ENDPOINT_SHORT = `https://api.spotify.com/v1/me/top/tracks`;
-const TOP_TRACKS_ENDPOINT_MEDIUM = `https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=medium_term`;
-export const TOP_TRACKS_ENDPOINT_LONG = `https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=long_term`;
-const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+// tracks
+const TOP_TRACKS_ENDPOINT_SHORT = `https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=short_term`; //last 4 weeks
+const TOP_TRACKS_ENDPOINT_MEDIUM = `https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=medium_term`; // last 6 months
+export const TOP_TRACKS_ENDPOINT_LONG = `https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=long_term`; // several years of data
 
-// /**
-//  * Get a User's Top Tracks
-//  * https://developer.spotify.com/documentation/web-api/reference/personalization/get-users-top-artists-and-tracks/
-//  */
-// export const getTopTracksShort = async (session) => {
-//   const res = await fetch(TOP_TRACKS_ENDPOINT_SHORT, {
-//     headers: {
-//       Authorization: `Bearer ${session.accessToken}`,
-//     },
-//   }).then((res) => res.json());
+// artist
+export const TOP_ARTISTS_ENDPOINT_SHORT = `https://api.spotify.com/v1/me/top/artists?limit=50&time_range=short_term`; //last 4 weeks
 
-//   return res;
-// };
+// spotify api endpoint to get users top albums
+export const USER_PLAYLIST_ENDPOINT = `https://api.spotify.com/v1/me/playlists`;
 
-// エンドポイント"TOP_TRACKS_ENDPOINT_SHORT"からトラック情報を取得する
-// トラック情報は"res"に格納される
-// トラック情報を返す
-// axiosを使用する場合
-export const getTopTracksShort = async (session) => {
+const fetcher = async (url, session) => {
   const res = await axios
-    .get(TOP_TRACKS_ENDPOINT_SHORT, {
+    .get(url, {
       headers: {
         Authorization: `Bearer ${session.accessToken}`,
       },
     })
-    .then((res) => res.data);
+    .then((r) => r.data);
 
   return res;
+};
+
+// エンドポイント"TOP_TRACKS_ENDPOINT_SHORT"からトラック情報を取得する
+//  * Get a User's Top Tracks
+//  * https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+//  */
+export const getTopTracksShort = async (session) => {
+  return fetcher(TOP_TRACKS_ENDPOINT_SHORT, session);
+};
+
+/**
+ * Get a User's Top Artists
+ * https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+ */
+export const getTopArtistsShort = async (session) => {
+  return fetcher(TOP_ARTISTS_ENDPOINT_SHORT, session);
+};
+
+/**
+ * Get a User's Top Artists
+ * https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+ */
+export const getUserPlayList = async (session) => {
+  return fetcher(USER_PLAYLIST_ENDPOINT, session);
 };
