01b523b06354fe4088ed81e98affb45085504e3e
ayakamada <sdkamada@gmail.com>
2023-04-07 14:42:27 +0900

    Login component作成

--- src/pages/playlist.jsx
+++ src/pages/playlist.jsx
@@ -1,17 +1,13 @@
 // import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import { getSession, getServerSession, useSession, signIn, signOut } from "next-auth/react";
-import React, { useState, useEffect, ReactNode } from "react";
-
-import { NextPage, GetServerSideProps } from "next";
-import Link from "next/link";
-import Image from "next/image";
+import { getSession, useSession } from "next-auth/react";
+import React from "react";
 
 import Layout from "@/components/layouts/layout";
+import Login from "@/components/Login";
 
-import { isAuthenticated } from "@/lib/utils/isAuthenticated";
 import { getUserPlayList } from "@/lib/spotify";
 
-import Index from "@/components/page/playlist/Index";
+import PlayListIndex from "@/components/page/playlist/Index";
 
 const PlayList = ({ UserPlayList }) => {
   const { data: session, status } = useSession();
@@ -20,26 +16,12 @@ const PlayList = ({ UserPlayList }) => {
 
   return (
     <>
-      {!session && (
-        <>
-          <main className="flex h-screen w-screen items-center justify-center bg-black text-center">
-            <div>
-              <h1 className="font-sans tracking-wide text-white">Spotify App</h1>
-              <button
-                onClick={() => signIn("spotify", { callbackUrl: "/" })}
-                className="mt-5 rounded-full bg-green p-4 font-sans tracking-wide text-white"
-              >
-                LOGIN TO SPOTIFY
-              </button>
-            </div>
-          </main>
-        </>
-      )}
+      {!session && <Login />}
 
       {status === "authenticated" && (
         <>
           <Layout>
-            <Index playlists={UserPlayList.items} />
+            <PlayListIndex playlists={UserPlayList.items} />
           </Layout>
         </>
       )}
