88192d758e98c255bdef5a21de4b13170022bd3a
ayakamada <sdkamada@gmail.com>
2023-04-10 17:50:26 +0900

    Update index.js

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -4,3 +4,51 @@ export const formatDuration = (ms) => {
   const seconds = ((ms % 60000) / 1000).toFixed(0);
   return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 };
+
+
+export const parsePitchClass = (note) => {
+  let key = note;
+
+  switch (note) {
+    case 0:
+      key = "C";
+      break;
+    case 1:
+      key = "D♭";
+      break;
+    case 2:
+      key = "D";
+      break;
+    case 3:
+      key = "E♭";
+      break;
+    case 4:
+      key = "E";
+      break;
+    case 5:
+      key = "F";
+      break;
+    case 6:
+      key = "G♭";
+      break;
+    case 7:
+      key = "G";
+      break;
+    case 8:
+      key = "A♭";
+      break;
+    case 9:
+      key = "A";
+      break;
+    case 10:
+      key = "B♭";
+      break;
+    case 11:
+      key = "B";
+      break;
+    default:
+      return null;
+  }
+
+  return key;
+};
