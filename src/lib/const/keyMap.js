88af60139b618200832e48fe3642b7945a32b788
ayakamada <sdkamada@gmail.com>
2023-04-11 11:01:27 +0900

    配列で管理

---
+++ src/lib/const/keyMap.js
@@ -0,0 +1,1 @@
+export const keyMap = ["C", "C♯/D♭", "D", "D♯/E♭", "E", "F", "F♯/G♭", "G", "G♯/A♭", "A", "A♯/B♭", "B"];
--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,3 +1,6 @@
+import { keyMap } from "@/lib/const/keyMap";
+
+
 // Format milliseconds into MM:SS
 export const formatDuration = (ms) => {
   const minutes = Math.floor(ms / 60000);
@@ -6,49 +9,8 @@ export const formatDuration = (ms) => {
 };
 
 
-export const parsePitchClass = (note) => {
-  let key = note;
 
-  switch (note) {
-    case 0:
-      key = "C";
-      break;
-    case 1:
-      key = "D♭";
-      break;
-    case 2:
-      key = "D";
-      break;
-    case 3:
-      key = "E♭";
-      break;
-    case 4:
-      key = "E";
-      break;
-    case 5:
-      key = "F";
-      break;
-    case 6:
-      key = "G♭";
-      break;
-    case 7:
-      key = "G";
-      break;
-    case 8:
-      key = "A♭";
-      break;
-    case 9:
-      key = "A";
-      break;
-    case 10:
-      key = "B♭";
-      break;
-    case 11:
-      key = "B";
-      break;
-    default:
-      return null;
-  }
-
-  return key;
-};
+//渡された引数とkeyMapのkeyを比較して、一致したらkeyMapのvalueを返す
+export const getKey = (key) => {
+  return keyMap[key];
+}
