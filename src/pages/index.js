7c3925e9ef8d9e12c82aea8b84e4417b80c20aa6
ayakamada <sdkamada@gmail.com>
2023-04-13 20:34:13 +0900

    layoutに記述を移動

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,21 @@
 // import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import React from "react";
-import { getSession, useSession, signOut } from "next-auth/react";
+import { getSession, signOut } from "next-auth/react";
 
 import Layout from "@/components/layouts/Layout";
-import Login from "@/components/Login";
+
 import UserSummary from "@/components/page/index/UserSummary";
 
 import { getTopTracksShort, getTopArtistsShort } from "@/lib/spotify";
 
 const Home = ({ topTracks, topArtists }) => {
-  const { data: session, status } = useSession();
-  const image_url = session?.user?.image;
-
-  // console.log(topTracks);
-
   return (
     <>
-      {!session && <Login />}
-
-      {status === "authenticated" && (
-        <>
-          <Layout>
-            <section className="pt-16 text-center">
-              {/* <img src={image_url} className="mx-auto h-32 w-32 rounded-full object-cover" alt="profil" /> */}
-              <h1 className="py-5 text-5xl">{session?.user?.name} </h1>
-              <div className="py-5">
-                <button
-                  className="bg-black-700 hover:bg-black-700 focus:ring-black-500 focus:ring-offset-black-200 rounded-full border-2 border-gray-900 py-2 px-4 text-center text-base font-semibold text-black transition duration-200 ease-in hover:bg-gray-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
-                  onClick={() => signOut()}
-                >
-                  Log out
-                </button>
-              </div>
-              <UserSummary tracks={topTracks.items} artists={topArtists.items} />
-            </section>
-          </Layout>
-        </>
-      )}
+      <Layout>
+        <section className="">
+          <UserSummary tracks={topTracks.items} artists={topArtists.items} />
+        </section>
+      </Layout>
     </>
   );
 };
