49d1613c998b3d47b935f05a5191c8914538e102
ayakamada <sdkamada@gmail.com>
2023-04-13 20:34:31 +0900

    headerで管理に変更

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,7 +1,18 @@
-export default function Header({  }) {
+export default function Header({ profileImage, userName }) {
   return (
     <>
-      <header></header>
+      <header className="pt-16 text-center mb-20">
+        <img src={profileImage} className="mx-auto h-32 w-32 rounded-full object-cover" alt="profil" />
+        <h1 className="py-5 text-5xl">{userName} </h1>
+        <div className="">
+          <button
+            className="block  mx-auto bg-black-700 hover:bg-black-700 focus:ring-black-500 focus:ring-offset-black-200 rounded-full border-2 border-white py-2 px-4 text-center text-sm text-white transition duration-200 ease-in hover:bg-white hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2"
+            onClick={() => signOut()}
+          >
+            LOGOUT
+          </button>
+        </div>
+      </header>
     </>
   );
 }
