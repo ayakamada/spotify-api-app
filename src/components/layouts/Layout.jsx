f2fed46cf18d8cef1aa7ad7dcbfd8d711aa188e7
ayakamada <sdkamada@gmail.com>
2023-04-13 20:34:01 +0900

    layoutでsessionによる表示切り替えを管理

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,14 +1,30 @@
+import { useSession } from "next-auth/react";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
+import Login from "@/components/Login";
+import Loading from "@/components/Loading";
+
 export default function Layout({ children }) {
+  const { data: session, status } = useSession();
+  const imageUrl = session?.user?.image;
+  const userName = session?.user?.name;
+
+  if (status === "loading") {
+    return <Loading />;
+  }
   return (
     <>
-      <div className="bg-black text-white h-full">
-        <Header />
-        <main>{children}</main>
-        <Footer />
-      </div>
+      {!session && <Login />}
+
+      {status === "authenticated" && (
+        <div className="bg-black text-white h-full">
+          <Header profileImage={imageUrl} userName={userName} />
+          <main>{children}</main>
+          <Footer />
+        </div>
+      )}
     </>
   );
 }
