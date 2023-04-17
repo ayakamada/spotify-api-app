694700ab08a043e3cf8fe0167460ca26d36237c1
ayakamada <sdkamada@gmail.com>
2023-04-17 20:43:45 +0900

    update styles

--- src/components/page/playlist/Index.jsx
+++ src/components/page/playlist/Index.jsx
@@ -4,19 +4,21 @@ import Link from "next/link";
 export default function PlayListIndex({ playlists }) {
   // console.log(playlists);
   return (
-    <div className="grid grid-cols-5">
-      {playlists.map((list) => (
-        <div key={list.id} className=" flex items-center">
-          <Link href={`/playlist/${list.id}`}>
-            <div className="mr-6 relative w-24 h-24">
-              <Image src={list.images[0].url} alt="" fill className="object-contain mx-auto w-24 h-24 " />
+    <div className="pb-32">
+      <div className="grid grid-cols-5 gap-8 w-[90%] mx-auto">
+        {playlists.map((list) => (
+          <div key={list.id} className="">
+            <Link href={`/playlist/${list.id}`}>
+              <div className="mr-6 relative w-48 h-48">
+                <Image src={list.images[0].url} alt="" fill className="object-contain mx-auto" />
+              </div>
+            </Link>
+            <div>
+              <h3 className="text-sm tracking-wide font-bold my-4">{list.name}</h3>
             </div>
-          </Link>
-          <div>
-            <h3>{list.name}</h3>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
