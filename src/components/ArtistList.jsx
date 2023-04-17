35d2a4ce2d513985bb3e90301118f4aa6641635e
ayakamada <sdkamada@gmail.com>
2023-04-17 20:45:29 +0900

    Update ArtistList.jsx

--- src/components/ArtistList.jsx
+++ src/components/ArtistList.jsx
@@ -3,23 +3,24 @@ import Image from "next/image";
 export default function ArtistList({ artists }) {
   return (
     <>
-      {artists.map((artist) => (
-        <div key={artist.id} className=" flex items-center">
-          <div className="mr-6 ">
-            <Image
-              src={artist.images[0].url}
-              alt=""
-              responsive
-              className="object-cover mx-auto rounded-full w-24 h-24 "
-              width={artist.images[0].width}
-              height={artist.images[0].height}
-            />
+      {artists.length > 0 &&
+        artists.map((artist) => (
+          <div key={artist.id} className=" flex items-center mx-auto  mb-4">
+            <div className="mr-6 ">
+              <Image
+                src={artist.images[0].url}
+                alt=""
+                responsive
+                className="object-cover mx-auto rounded-full w-24 h-24 "
+                width={artist.images[0].width}
+                height={artist.images[0].height}
+              />
+            </div>
+            <div>
+              <h3 className="font-bold tracking-wider">{artist.name}</h3>
+            </div>
           </div>
-          <div>
-            <h3>{artist.name}</h3>
-          </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 }
