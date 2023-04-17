f15723825c76a98d7b45b0d92c902ba1eb188192
ayakamada <sdkamada@gmail.com>
2023-04-17 20:43:02 +0900

    title修正

--- src/components/page/index/UserSummary.jsx
+++ src/components/page/index/UserSummary.jsx
@@ -1,18 +1,29 @@
+import Link from "next/link";
 import Image from "next/image";
 import TrackList from "@/components/TrackList";
 import ArtistList from "@/components/ArtistList";
 
+function TitleLink({ title, linkUrl }) {
+  return (
+    <div className="flex justify-between">
+      <h2 className="text-center font-bold mb-10 text-xl leading-relaxed tracking-wide">{title}</h2>
+      {/* <Link href={linkUrl}>
+        <div className="text-sm font-light shrink-0 border rounded-full inline-block px-2 py-1">See More</div>
+      </Link> */}
+    </div>
+  );
+}
 
 export default function UserSummary({ tracks, artists }) {
   return (
     <div className="grid grid-cols-2">
-      <section className=" ">
-        <h2>Top Tracks of Short Term</h2>
+      <section className=" w-4/5 mx-auto">
+        <TitleLink title="Top Tracks" linkUrl="/tracks" />
         <TrackList tracks={tracks} />
       </section>
 
-      <section className=" ">
-        <h2>Top artists of Short Term</h2>
+      <section className=" w-4/5 mx-auto">
+        <TitleLink title="Top Artists" linkUrl="/artists" />
         <ArtistList artists={artists} />
       </section>
     </div>
