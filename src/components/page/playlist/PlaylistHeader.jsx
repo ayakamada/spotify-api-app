b112309583dd2222c330a1d80ea6676e43b6716e
ayakamada <sdkamada@gmail.com>
2023-04-18 22:03:34 +0900

    トラック数を表示、style update

--- src/components/page/playlist/PlaylistHeader.jsx
+++ src/components/page/playlist/PlaylistHeader.jsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function PlayListHeader({ playlist }) {
-  // console.log(playlists);
   return (
-    <div className=" flex items-center">
-      <div className="mr-6 relative w-24 h-24">
+    <div className=" flex items-center tracking-wide">
+      <div className="mr-6 relative w-60 h-60">
         <Image src={playlist.images[0].url} alt="" fill className="object-contain mx-auto w-24 h-24 " />
       </div>
       <div>
-        <h3>{playlist.name}</h3>
-        <div>{playlist.owner.display_name}</div>
+        <h3 className="font-bold text-2xl">{playlist.name}</h3>
+        <div className="text-gray-400 mb-2">BY {playlist.owner.display_name}</div>
+        <div className="text-gray-200 text-sm">{playlist.tracks.total} tracks</div>
       </div>
     </div>
   );
