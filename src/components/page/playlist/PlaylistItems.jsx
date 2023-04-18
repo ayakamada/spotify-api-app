f216acf9051fc74b34af72ecfa6d834b8d7b131f
ayakamada <sdkamada@gmail.com>
2023-04-18 22:03:47 +0900

    style update

--- src/components/page/playlist/PlaylistItems.jsx
+++ src/components/page/playlist/PlaylistItems.jsx
@@ -8,14 +8,14 @@ export default function PlayListItems({ tracks }) {
   return (
     <div>
       {tracks.items.map((item) => (
-        <div key={item.track.id} className=" flex items-center">
-          <div className="mr-6 relative w-36 h-36">
-            <Image src={item.track.album.images[0].url} alt="" fill className="object-contain mx-auto w-36 h-36 " />
+        <div key={item.track.id} className=" flex items-start">
+          <div className="mr-6 relative w-24 h-24 shrink-0">
+            <Image src={item.track.album.images[0].url} alt="" fill className="object-contain mx-auto " />
           </div>
           <div>
-            <h3>{item.track.name}</h3>
-            <div>{item.track.artists[0].name}</div>
-            <div>{formatDuration(item.track.duration_ms)}</div>
+            <h3 className="font-bold">{item.track.name}</h3>
+            <div className="text-gray-300">{item.track.artists[0].name}</div>
+            <div className="text-xs text-gray-400">{formatDuration(item.track.duration_ms)}</div>
           </div>
         </div>
       ))}
