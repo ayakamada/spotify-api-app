fddecc059ac3c0350af26d9e92b10ebbdc9ee02e
ayakamada <sdkamada@gmail.com>
2023-04-19 21:58:58 +0900

    Update PlaylistHeader.jsx

--- src/components/page/playlist/PlaylistHeader.jsx
+++ src/components/page/playlist/PlaylistHeader.jsx
@@ -3,15 +3,17 @@ import Link from "next/link";
 
 export default function PlayListHeader({ playlist }) {
   return (
-    <div className=" flex items-center tracking-wide">
-      <div className="mr-6 relative w-60 h-60">
-        <Image src={playlist.images[0].url} alt="" fill className="object-contain mx-auto w-24 h-24 " />
+    <>
+      <div className="tracking-wide bg-gray-900 p-4 rounded-lg h-full ">
+        <div className="mr-6 relative w-44 h-44 mb-3">
+          <Image src={playlist.images[0].url} alt="" fill className="object-contain mx-auto " />
+        </div>
+        <div>
+          <h3 className="font-bold text-2xl">{playlist.name}</h3>
+          <div className="text-gray-400 mb-2">BY {playlist.owner.display_name}</div>
+          <div className="text-gray-200 text-sm">{playlist.tracks.total} tracks</div>
+        </div>
       </div>
-      <div>
-        <h3 className="font-bold text-2xl">{playlist.name}</h3>
-        <div className="text-gray-400 mb-2">BY {playlist.owner.display_name}</div>
-        <div className="text-gray-200 text-sm">{playlist.tracks.total} tracks</div>
-      </div>
-    </div>
+    </>
   );
 }
