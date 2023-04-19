57b7825bbf247e6ec1c3aa98f6ed538487b84dac
ayakamada <sdkamada@gmail.com>
2023-04-19 21:59:01 +0900

    Update PlaylistItems.jsx

--- src/components/page/playlist/PlaylistItems.jsx
+++ src/components/page/playlist/PlaylistItems.jsx
@@ -6,22 +6,19 @@ import { formatDuration } from "@/lib/utils/index";
 export default function PlayListItems({ tracks }) {
   //
   return (
-    <div>
+    <div className="w-full">
       {tracks.items.map((item) => (
-        <div key={item.track.id} className=" flex items-start">
-          <div className="mr-6 relative w-24 h-24 shrink-0">
+        <div key={item.track.id} className=" flex items-center mb-2">
+          <div className="mr-6 relative w-12 h-12 shrink-0">
             <Image src={item.track.album.images[0].url} alt="" fill className="object-contain mx-auto " />
           </div>
           <div>
             <h3 className="font-bold">{item.track.name}</h3>
             <div className="text-gray-300">{item.track.artists[0].name}</div>
-            <div className="text-xs text-gray-400">{formatDuration(item.track.duration_ms)}</div>
           </div>
+          <div className="text-xs text-gray-400 ml-auto">{formatDuration(item.track.duration_ms)}</div>
         </div>
       ))}
-      <div>
-        {tracks.items.length} / {tracks.total}
-      </div>
     </div>
   );
 }
