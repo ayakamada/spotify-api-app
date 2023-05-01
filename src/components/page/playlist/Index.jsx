3fd588fa137e413a929620cf63dc702b3f2a5234
ayakamada <sdkamada@gmail.com>
2023-05-01 21:09:29 +0900

    responsiveに対応

--- src/components/page/playlist/Index.jsx
+++ src/components/page/playlist/Index.jsx
@@ -5,16 +5,23 @@ export default function PlayListIndex({ playlists }) {
   // console.log(playlists);
   return (
     <div className="pb-32">
-      <div className="grid grid-cols-5 gap-8 w-[90%] mx-auto">
+      <div className="grid grid-cols-2 sm:grid-cols-5 gap-8 w-[90%] mx-auto">
         {playlists.map((list) => (
-          <div key={list.id} className="">
+          <div key={list.id} className="w-full h-full">
             <Link href={`/playlist/${list.id}`}>
-              <div className="mr-6 relative w-48 h-48">
-                <Image src={list.images[0].url} alt="" fill className="object-contain mx-auto" />
+              <div className="sm:mr-6 relative w-full">
+                <Image
+                  src={list.images[0].url}
+                  alt=""
+                  responsive
+                  width={list.images[0]?.width || 640}
+                  height={list.images[0]?.height || 640}
+                  className=" mx-auto"
+                />
               </div>
             </Link>
             <div>
-              <h3 className="text-sm tracking-wide font-bold my-4">{list.name}</h3>
+              <h3 className="text-sm tracking-wide font-bold my-4 text-center">{list.name}</h3>
             </div>
           </div>
         ))}
